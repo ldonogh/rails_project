229252de4ad247222b49d0609fb75dde9f20db46
ldonogh <linds91@gmail.com>
2013-12-02 21:02:26 -0600

    cleaned up controllers

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,11 +3,6 @@ class GenresController < ApplicationController
   # GET /genres.json
   def index
     @genres = Genre.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @genres }
-    end
   end
 
   # GET /genres/1
@@ -16,82 +11,11 @@ class GenresController < ApplicationController
     @genre = Genre.find(params[:genre_id])
     @genres = Genre.all
     @books = @genre.books #<-- THIS IS IMPORTANT
-
-    respond_to do |format|
-      format.html # genre.html.erb
-      format.json { render json: @genre }
-    end
   end
 
   def book
     @genre = Genre.find(params[:genre_id])
     @genres = Genre.all
-
-    respond_to do |format|
-      format.html # genre.html.erb
-      format.json { render json: @genre }
-    end
   end
 
-  # GET /genres/new
-  # GET /genres/new.json
-  def new
-    @genre = Genre.new
-    @genres = Genre.order(:name)
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @genre }
-    end
-  end
-
-  # GET /genres/1/edit
-  def edit
-    @genre = Genre.find(params[:id])
-    @genres = Genre.order(:name)
-  end
-
-  # POST /genres
-  # POST /genres.json
-  def create
-    @genre = Genre.new(params[:genre])
-
-    respond_to do |format|
-      if @genre.save
-        format.html { redirect_to @genre, notice: 'Genre was successfully created.' }
-        format.json { render json: @genre, status: :created, location: @genre }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @genre.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /genres/1
-  # PUT /genres/1.json
-  def update
-    @genre = Genre.find(params[:id])
-
-    respond_to do |format|
-      if @genre.update_attributes(params[:genre])
-        format.html { redirect_to @genre, notice: 'Genre was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @genre.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /genres/1
-  # DELETE /genres/1.json
-  def destroy
-    @genre = Genre.find(params[:id])
-    @genre.destroy
-
-    respond_to do |format|
-      format.html { redirect_to genres_url }
-      format.json { head :no_content }
-    end
-  end
-end
+  
